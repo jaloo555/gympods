546f8a1cac3eb6139337be596349490dde69116d
Jason Chen <superj63@hotmail.com>
2018-11-18 21:51:16 -0800

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,30 +1,55 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons'
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
 
 const Footer = (props) => (
     <footer id="footer">
         <div className="inner">
-            <ul className="icons">
-                <li><a href="#"><FontAwesomeIcon icon={}/><span className="label">Facebook</span></a></li>
-                <li><a href="#"><FontAwesomeIcon icon={}/><span className="label">Instagram</span></a></li>
-            </ul>
-            <ul className="copyright">
-                <li>&copy; Gympods</li><li>Design: <a href="https://github.com/jaloo555">jaloo55</a></li>
-            </ul>
+            <div>
+                <ul>
+                    <li><a href="mailto:chisanch@outlook.com" className="icons"><FontAwesomeIcon icon={faEnvelope}/></a></li>
+                    <li><a href="#" className="icons"><FontAwesomeIcon icon={faFacebook}/></a></li>
+                    <li><a href="#" className="icons"><FontAwesomeIcon icon={faInstagram}/></a></li>
+                    <li><a href="https://github.com/jaloo555" className="icons"><FontAwesomeIcon icon={faGithub}/></a></li>
+                </ul>
+            </div>
+            <div className="copyright">
+                <p>&copy; Gympods Ltd.</p>
+            </div>
         </div>
         <style jsx>{`
-            .footer {
-                background-color: #000000;
-                display: flex;
+            #footer {
+                background-color: #080808;
+                color: white;
             }
             .icons {
+                text-decoration: none;
+                color: white;
+                font-size: 30px;
+                margin: 0;
+                padding-right: 20px;
+                padding-left: 20px;
+            }
+            .inner {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-direction: column;
+                margin-left: auto;
+                margin-right: auto;
+            }
+            ul {
                 display: flex;
                 list-style: none;
-                justify-content: center;
+                
             }
             .copyright {
                 display: flex;
                 list-style: none;
+                margin-left: auto;
+                margin-right: auto;
+                font-style: italic;
+                color: gray;
             }
         `}
         </style>
